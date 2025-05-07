c76fa441c65ebd9a130b832a004dd2c0d7c9411a
iamrajveers <rajveer.singh@blinksigns.com>
2025-05-07 20:01:12 +0500

    Server basic

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
-import express from "express";
-import dotenv from 'dotenv';
+import express from "express";   // express js frame work of node js with help we build Api 
+import dotenv from 'dotenv'; // dotenv we  can we add senstive info env file
+const app = express();   // create the express app
+dotenv.config() // load .env veriables
 
-const app = express();
-dotenv.config()
+const port =  process.env.PORT || 3000;   // set the port
 
-const port =  process.env.PORT || 3000;
-
-app.get('/' , (req, res) => {
+app.get('/' , (req, res) => {    // create route
 res.status(200).send("Hello World")
 })
 
-app.listen(port, () => {
+app.listen(port, () => {    // start server
     console.log(`Server is running on ${port}`);
 })
+
