0ab0cf68c5341e93c04c8c1d0ef3806da5d18999
iamrajveers <rajveer.singh@blinksigns.com>
2025-05-08 22:29:17 +0500

    new  routes

---
+++ src/app.js
@@ -0,0 +1,10 @@
+// import express from "express"
+// const app = express();
+// app.use(express.json());
+
+// // Example route
+// app.get('/', (req, res) => {
+//   res.send('Hello from Node on ldocafflhost!');
+// });
+
+// module.exports = app;
